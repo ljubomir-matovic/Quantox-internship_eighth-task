b65a33baf6b16a5a925ffab3225621939af42069
ljubomir-matovic <ljubomir.matovic@quantoxtechnology.com>
2021-10-30 15:23:29 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var formHandler = function () {
+    this.forms = document.querySelectorAll("form");
+    this.inputs = document.querySelectorAll("input[type='text']");
+    this.toggleButton = (e) => {
+        let el = e.target;
+        let button = el.parentElement.querySelector("button");
+        if (el.value == "") {
+            button.style.display = "none";
+        } else {
+            button.style.display = "block";
+        }
+    };
+    this.inputs.forEach((i) => i.addEventListener("keyup", this.toggleButton));
+    this.forms.forEach((f) =>
+        f.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let input = e.target.querySelector("input[name='title']");
+            let value = input.value;
+            input.value = "";
+            this.toggleButton({ target: input });
+            console.log(value);
+        })
+    );
+    return this;
+}.call({});
